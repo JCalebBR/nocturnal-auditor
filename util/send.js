0ef33495b9cdc6e655a494b81e6c7080e52cb8b5
Josué Caleb <josshuacaleb@gmail.com>
2022-06-29 21:03:21 -0300

    Fix send command; Added alternate method to send, based on missing fields on some logs

--- util/send.js
+++ util/send.js
@@ -1,7 +1,14 @@
 const { auditChannel } = require('../config.json');
 
 const send = async (message, data, Log) => {
-    await message.client.channels.cache.get(auditChannel[message.guildId]).send(data).then(Log.debug(`Data sent successfully!`));
+    try {
+        await message.client.channels.cache.get(auditChannel[message.guildId]).send(data).then(Log.debug(`Data sent successfully!`));
+    } catch (error) {
+        Log.error(`Error when sending | ${error}`);
+        Log.debug(`Attempting alternate method...`);
+        await message.client.channels.cache.get(auditChannel[message.guild.id]).send(data).then(Log.debug(`Data sent successfully!`));
+    }
+
 };
 
 exports.send = send;
